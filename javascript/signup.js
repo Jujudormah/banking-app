379562515ab83f9fb7868b611e864c08bfc19eec
Jujudormah <gustavo_diaz14@yahoo.com>
2025-12-08 21:13:00 -0800

    Finalized CSS and fixed links/logo

--- javascript/signup.js
+++ javascript/signup.js
@@ -3,6 +3,12 @@ const signUpForm = document.querySelector('.signup-form');
 const formName = document.querySelector('#form-fullname');
 const formPassword = document.querySelector('#form-password');
 const successBanner = document.querySelector('.success-banner');
+const formFailure = document.querySelector('.form-failure');
+const logo = document.querySelector('#logo');
+
+logo.addEventListener('click', function (even) {
+  window.location.href = 'landing.html';
+});
 
 signUpForm.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -30,11 +36,16 @@ signUpForm.addEventListener('submit', function (event) {
     console.log(user);
 
     successBanner.style.transform = 'translateY(0)';
+    formFailure.style.display = 'none';
     setTimeout(() => {
       successBanner.style.transform = 'translateY(-100%)';
     }, 2000);
+    setTimeout(() => {
+      window.location.href = 'landing.html';
+    }, 3000);
   } else {
     console.log('failed');
+    formFailure.style.display = 'block';
   }
   formName.value = '';
   formPassword.value = '';
